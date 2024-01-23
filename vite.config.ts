b48f4d3e2107ca45f9ba7506256c0d7fdedf583f
bagou450 <bagou450roro@gmail.com>
2024-01-23 09:45:02 +0100

    Correct environment variable name in vite.config.ts
    The base configuration in vite.config.ts has been updated to use the correct environment variable for determining if the application is hosted on GitHub Pages. This ensures correct routing whether the application is hosted locally or on GitHub Pages.

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  base: process.env.GITHUB_PAGES_RG_FJKZ65GDVZMf ? '/CloudflareDNSManager/' : '/',
+  base: process.env.GHUB_PAGES_RG_FJKZ65GDVZMf ? '/CloudflareDNSManager/' : '/',
 })
